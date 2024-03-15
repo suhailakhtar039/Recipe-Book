e1a2535d321be480962a4234fa6db7ed4e274d26
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-15 16:36:57 +0530

    recipes list created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,26 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Momos', 'Bad for health', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'),new Recipe('Salad', 'Good for health', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341')
-  ]
+  
+  recipes: Recipe[]
+  constructor(private recipeService:RecipeService){}
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();  
+  }
+
 
   onRecipeSelected(recipe:Recipe){
     this.recipeWasSelected.emit(recipe);
   }
-
-  // ngOnInit(): void {
-  //   throw new Error('Method not implemented.');
-  // }
+  
+  
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,12 @@
-export class RecipeService{
-    
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('Momos', 'Bad for health', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'), new Recipe('Salad', 'Good for health', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341')
+    ]
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
 }
