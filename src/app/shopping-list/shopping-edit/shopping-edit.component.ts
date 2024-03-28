944825f58d7992787fd8085393b928b072fbdebb
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-28 11:52:36 +0530

    clear button functionalities added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -48,6 +48,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     form.reset()
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
   ngOnDestroy(): void {
     console.log("inside on destroy")
     this.slForm.reset();
