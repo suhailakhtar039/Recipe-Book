dc1b30c593c1dc483f9b1d89a15cae91c6ed7d79
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-29 12:19:28 +0530

    recipes edit completed

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css',
-  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  constructor(private recipeService:RecipeService){}
+  constructor(){}
 
   ngOnInit(): void {
       
