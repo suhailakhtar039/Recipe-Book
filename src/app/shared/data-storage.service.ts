f9d4db6f50a3feee70dfd903a4f2f6f08354d0ff
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-06 15:46:55 +0530

    save recipes data called

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+import { RecipeService } from "../recipes/recipe.service";
+
 @Injectable({providedIn:'root'})
 export class DataStorageService{
-    constructor(private http:HttpClient){}
+    constructor(private http:HttpClient, private recipeService: RecipeService){}
+
+    storeRecipe(){
+        const recipes = this.recipeService.getRecipes();
+        this.http
+            .put('https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json', recipes)
+            .subscribe(response =>{
+                console.log(response)
+            })
+    }
+
 }
