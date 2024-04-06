7a8ed24cb9262696ae086983fcfd94a558f68477
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-06 16:58:09 +0530

    bug taken care of

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -18,16 +18,17 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return  this.http
             .get<Recipe[]>('https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json')
             .pipe(map(recipes => {
                 return recipes.map(recipe => {
                     return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
                 })
-            }))
-            .subscribe(response => {
-                this.recipeService.setRecipes(response)
+            }),
+            tap(recipes =>{
+                this.recipeService.setRecipes(recipes)
             })
+            )
     }
 
 }
