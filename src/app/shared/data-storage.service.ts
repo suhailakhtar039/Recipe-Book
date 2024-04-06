b622150dd32a23d2fb32a263a3b7f73ee9a316b9
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-06 16:33:20 +0530

    empty array of ingredients added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,23 +3,29 @@ import { Injectable } from "@angular/core";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
-@Injectable({providedIn:'root'})
-export class DataStorageService{
-    constructor(private http:HttpClient, private recipeService: RecipeService){}
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipeService: RecipeService) { }
 
-    storeRecipes(){
+    storeRecipes() {
         const recipes = this.recipeService.getRecipes();
         this.http
             .put('https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json', recipes)
-            .subscribe(response =>{
+            .subscribe(response => {
             })
     }
 
-    fetchRecipes(){
+    fetchRecipes() {
         this.http
             .get<Recipe[]>('https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json')
-            .subscribe(response=>{
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+                })
+            }))
+            .subscribe(response => {
                 this.recipeService.setRecipes(response)
             })
     }
