3a965cbbbe213b67828db756426aed9d28715dac
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-07 16:54:26 +0530

    login observable created

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
-interface AuthResponseData{
+export interface AuthResponseData{
     idToken:string;
     email:string;
     refreshToken:string;
     expiresIn:string;
     localId:string;
+    registered?:boolean;
 }
 
 @Injectable({providedIn:'root'})
@@ -34,5 +35,13 @@ export class AuthService{
                     return throwError(errorMessage);
                 }
             ))
+    }
+
+    logIn(email:string, password:string){
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC-51Fkt4S6wHT3PwtYNZhTaIuISvRu-w4',{
+            email:email,
+            password:password,
+            returnSecureToken: true
+        })
     }
 }
