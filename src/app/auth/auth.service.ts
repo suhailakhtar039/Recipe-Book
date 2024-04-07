6f62c9be8b52d17197d23b61b861ffd415663132
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-07 15:53:23 +0530

    loading spinner component created

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+interface AuthResponseData{
+    idToken:string;
+    email:string;
+    refreshToken:string;
+    expiresIn:string;
+    localId:string;
+}
+
 @Injectable({providedIn:'root'})
 export class AuthService{
     constructor(private http: HttpClient){}
     signup(email:string, password:string){
         return this.http
-            .post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC-51Fkt4S6wHT3PwtYNZhTaIuISvRu-w4',{
+            .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC-51Fkt4S6wHT3PwtYNZhTaIuISvRu-w4',{
                 email:email,
                 password:password,
                 returnSecureToken:true
