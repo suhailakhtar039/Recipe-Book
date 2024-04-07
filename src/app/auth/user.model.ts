9804d2f2c0b6a56ef8b396a175ea436430891b03
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-07 23:08:55 +0530

    user model created

---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,10 @@
+export class User{
+    constructor(public email: string, public id: string, private _token: string, private _tokenExpirateDate){}
+
+    get token(){
+        if(!this._tokenExpirateDate || new Date() > this._tokenExpirateDate){
+            return null;
+        }
+        return this._token;
+    }
+}
