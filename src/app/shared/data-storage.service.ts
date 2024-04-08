dd23676d5de0e39182b00f3bb3a5d03f98adba47
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-08 13:33:03 +0530

    fetch data completed in using exhaust map

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,34 +1,51 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
-import { map, tap } from "rxjs/operators";
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) { }
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
-    storeRecipes() {
-        const recipes = this.recipeService.getRecipes();
-        this.http
-            .put('https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json', recipes)
-            .subscribe(response => {
-            })
-    }
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {});
+  }
 
-    fetchRecipes() {
-        return  this.http
-            .get<Recipe[]>('https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json')
-            .pipe(map(recipes => {
-                return recipes.map(recipe => {
-                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
-                })
-            }),
-            tap(recipes =>{
-                this.recipeService.setRecipes(recipes)
-            })
-            )
-    }
-
+  fetchRecipes() {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        return this.http.get<Recipe[]>(
+          'https://ng-recipe-guide-8b69c-default-rtdb.firebaseio.com/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        );
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
 }
