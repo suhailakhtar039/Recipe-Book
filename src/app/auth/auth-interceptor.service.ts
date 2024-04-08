fd7a6e1a51aaae71a7a39e817ac09ad63153c67f
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-08 16:53:02 +0530

    auth interceptor completed

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -22,10 +22,13 @@ export class AuthInterceptorService implements HttpInterceptor {
     return this.authService.user.pipe(
       take(1),
       exhaustMap((user) => {
+        if(!user){
+            return next.handle(req);
+        }
         const modifiedReq = req.clone({
           params: new HttpParams().set('auth', user.token),
         });
-        return next.handle(req);
+        return next.handle(modifiedReq);
       })
     );
   }
