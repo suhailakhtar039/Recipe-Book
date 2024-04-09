8ce687774b4e7b1a4e4db7ab6d90a0d02b0162af
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-09 13:20:44 +0530

    auto logout implemented

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -1,8 +1,8 @@
 export class User{
-    constructor(public email: string, public id: string, private _token: string, private _tokenExpirateDate){}
+    constructor(public email: string, public id: string, private _token: string, private _tokenExpirationDate){}
 
     get token(){
-        if(!this._tokenExpirateDate || new Date() > this._tokenExpirateDate){
+        if(!this._tokenExpirationDate || new Date() > this._tokenExpirationDate){
             return null;
         }
         return this._token;
