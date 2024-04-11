df84823f18ab7ca5938c4804b10c109fe8f51e72
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-11 22:07:15 +0530

    increased linearity by creating AuthModule

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [CommonModule, FormsModule],
+})
+export class AuthModule {}
