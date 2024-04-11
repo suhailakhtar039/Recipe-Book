420660e1f1e05de1949dbeb96c7ea660a80caf3d
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-11 23:42:49 +0530

    lazy loding created for auth and shopping list

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -11,9 +11,7 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [ShoppingListComponent, ShoppingEditComponent],
   imports: [
     FormsModule,
-    RouterModule.forChild([
-      { path: 'shopping-list', component: ShoppingListComponent },
-    ]),
+    RouterModule.forChild([{ path: '', component: ShoppingListComponent }]),
     SharedModule,
   ],
 })
